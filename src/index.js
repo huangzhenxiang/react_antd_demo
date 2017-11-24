bda7bd040adde18f7254a29199c7403e638a318a
huangzhenxiang <huangzhenxiang@nle-tech.com>
2017-11-24 16:32:44 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+import Home from '@/components/Home/home.js'
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <HashRouter>
+    <App>
+      <Route path='/home' component={Home}></Route> 
+    </App>
+  </HashRouter>
+, document.getElementById('root'));
+registerServiceWorker();
